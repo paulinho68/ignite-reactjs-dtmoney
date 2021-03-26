5f0655333e8a72d47a27df3ab7fc5208676ff48b
Paulo Vitor <paulovitor2123@gmail.com>
2021-03-26 04:14:02 -0300

    Adicionando e configurando Axios e Mirage para requisições fake

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
 import * as Styles from './styles';
 
 export function TransactionsTable() {
+    useEffect(() => {
+        api.get('http://localhost:3000/api/transactions')
+            .then(response => console.log(response.data));
+    }, []);
+
     return (
         <Styles.Container>
             <table>
@@ -15,27 +22,27 @@ export function TransactionsTable() {
 
                 <tbody>
                     <tr>
-                        <td>Desenvolvimento de website</td>
+                        <td>Desenvolvimento de site</td>
                         <td>R$ 12.000</td>
-                        <td>Desenvolvimento</td>
+                        <td>Venda</td>
                         <td>25/03/2021</td>
                     </tr>
                     <tr>
                         <td>Desenvolvimento de website</td>
-                        <td>R$ 12.000</td>
+                        <td className="withdraw"> - R$ 59,00</td>
                         <td>Desenvolvimento</td>
                         <td>25/03/2021</td>
                     </tr>
                     <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000</td>
+                        <td>Aluguel do apartamento</td>
+                        <td className="withdraw">- R$ 1.200</td>
                         <td>Desenvolvimento</td>
                         <td>25/03/2021</td>
                     </tr>
                     <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$ 5.000</td>
-                        <td>Desenvolvimento</td>
+                        <td>Computador</td>
+                        <td className="deposit">R$ 5.000</td>
+                        <td>Venda</td>
                         <td>25/03/2021</td>
                     </tr>
                 </tbody>

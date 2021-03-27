6c80220f1096692f7fbf70735a246a575cf015d9
Paulo Vitor <paulovitor2123@gmail.com>
2021-03-27 03:13:06 -0300

    Adicionando modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import logo from '../../assets/dt_money.svg';
 import * as Styles from './styles';
 
-export function Header() {
+interface Props {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: Props) {
+
     return (
         <Styles.Container>
             <Styles.Content>
                 <img src={logo} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova Transação
                 </button>
             </Styles.Content>
